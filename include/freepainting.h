9db942cde36b53f9d30cf6094425794b969d3706
P1ckCheN <849566149@qq.com>
2021-06-20 22:13:58 +0800

    change 1 parm struct to 3 different

--- include/freepainting.h
+++ include/freepainting.h
@@ -12,21 +12,43 @@
 #ifndef FREEPAINTING_H__
 #define FREEPAINTING_H__
 
+
+#include "framework.h"
 #include "Resource.h"
 #include "parsingxml.h"
 
 // 定义参数结构体
-enum Parm
+enum ShapeParm
 {
 	TYPE_LINE,						///< 形状：直线	
 	TYPE_RECT,						///< 形状：矩形
 	TYPE_ELLIPSE,					///< 形状：椭圆
+};
+
+enum ColorParm
+{
 	TYPE_RED,						///< 颜色：红色
 	TYPE_BLUE,						///< 颜色：蓝色
 	TYPE_GREEN,						///< 颜色：绿色
+};
+
+enum LinewidthParm
+{
 	TYPE_THIN,						///< 粗细度：细
 	TYPE_MEDIUM,					///< 粗细度：中
 	TYPE_THICK						///< 粗细度：粗
 };
 
+struct DrawParm
+{
+	POINT ptBegin;
+	POINT ptEnd;
+	ShapeParm shape_parm;
+	//ColorParm color_parm;
+	//LinewidthParm LinewidthParm;
+	std::vector<int> color_parm;
+	int LinewidthParm;
+};
+void SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd,
+	std::vector<DrawParm>& draw_parm, ShapeParm type_shape, ColorParm type_color, LinewidthParm type_linewidth);
 #endif
