63e0ecfbd8a980c2bd8ffe140e170e593a3dfc3a
P1ckCheN <849566149@qq.com>
2021-06-20 22:25:57 +0800

    version 1.1, fix some problems

--- src/freepainting.cpp
+++ src/freepainting.cpp
@@ -3,17 +3,19 @@
  * \brief 程序入口
  *
  * 主程序
- *
+ * v1.1 
+ * (1) 修复绘图时起始点和终点不准的问题
+ * (2) 修复放大缩小窗口会擦除痕迹的问题
+ * (3) 更好地区分三种不同的绘图参数结构
  * \author 陈瑞佳
- * \version 1.0
- * \date 2021/06/17
+ * \version 1.1
+ * \date 2021/06/20
  */
 
-#include "framework.h"
+
 #include "freepainting.h"
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
 
 /**
  * @brief 窗口主程序
@@ -25,7 +27,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	MSG msg;
 	static HBRUSH hBrush;
 	WNDCLASS wndclass;
-	
+
 	LoadMenu(hInstance, TEXT("Menu"));
 	hBrush = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
 
@@ -39,7 +41,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	wndclass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
 	wndclass.lpszClassName = szAppName;
-	
+
 	if (!RegisterClassW(&wndclass))
 	{
 		MessageBox(NULL, TEXT("Program requires Windows NT!"),
@@ -62,111 +64,141 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	return msg.wParam;
 }
 
+void SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd, 
+	std::vector<DrawParm>& draw_parm, ShapeParm type_shape, ColorParm type_color, LinewidthParm type_linewidth)
+{
+	RECT rect; //使用参数NULL代替，表示整个屏幕
+	HPEN hpen;
+	int color[3];
+	int linewidth;
+
+	GetClientRect(hwnd, &rect);
+	DrawText(hdc, TEXT("Please move leftbutton to draw, click rightbutton to erase!"), 59, &rect, DT_CENTER | DT_SINGLELINE);
+	// TODO:完善异常处理机制
+	int flag_xml_read = ReadXmlFile(type_color, type_linewidth, color, &linewidth);
+	if (!flag_xml_read)
+	{
+		exit(-1);
+	}
+	DrawParm curr_parm;
+	curr_parm.ptBegin = ptBegin;
+	curr_parm.ptEnd = ptEnd;
+	curr_parm.shape_parm = type_shape;
+	for (int i = 0; i < 3; i++)
+	{
+		curr_parm.color_parm.push_back(color[i]);
+	}
+	curr_parm.LinewidthParm = linewidth;
+
+	draw_parm.emplace_back(curr_parm);
+	
+	// TODO: 用循环画图出现速度很慢，图数量多会出现画图帧数人眼可分辨的情况
+	for (int i = 0; i < draw_parm.size(); i++)
+	{
+		hpen = CreatePen(PS_SOLID, draw_parm[i].LinewidthParm, RGB(draw_parm[i].color_parm[0], draw_parm[i].color_parm[1], draw_parm[i].color_parm[2]));
+		SelectObject(hdc, hpen);
+		switch (draw_parm[i].shape_parm)
+		{
+			// TODO: 
+			// 2:实时显示画图过程，而非两个点相连
+		case TYPE_LINE:
+			MoveToEx(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, NULL);
+			LineTo(hdc, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
+			break;
+		case TYPE_RECT:
+			Rectangle(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
+			break;
+		case TYPE_ELLIPSE:
+			Ellipse(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
+			break;
+		default:
+			break;
+		}
+		DeleteObject(hpen);
+	}
+
+	
+}
+
 /**
  * @brief 消息回调函数
  */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
-	RECT rect;
 	PAINTSTRUCT ps;
 	int wmId, wmEvent;
-	static POINT ptBegin;
-	static POINT ptEnd;	
-	static Parm type_drawing = TYPE_LINE;								///< 默认形状：直线
-	static Parm type_color = TYPE_RED;									///< 默认色彩：红色
-	static Parm type_linewidth = TYPE_THIN;								///< 默认粗细度：细
-	static int color[3];
-	static int linewidth;
+	static POINT ptShapeBegin, ptShapeEnd;
+
+	static ShapeParm type_shape;								///< 默认形状：直线
+	static ColorParm type_color;								///< 默认色彩：红色
+	static LinewidthParm type_linewidth;						///< 默认粗细度：细
+	static std::vector<DrawParm> draw_parm;
 	int flag_xml_read = 0;
-	HPEN hPen;
 	switch (message)
 	{
 	case WM_LBUTTONDOWN:
-		GetCursorPos(&ptBegin);
+		ptShapeBegin.x = LOWORD(lParam);
+		ptShapeBegin.y = HIWORD(lParam);
+		//GetCursorPos(&ptShapeBegin); //获取坐标不准确
 		return 0;
 	case WM_LBUTTONUP:
-		GetCursorPos(&ptEnd);
-		hdc = GetDC(hwnd);
-		// TODO:完善异常处理机制
-		flag_xml_read = ReadXmlFile(type_color, type_linewidth, color, &linewidth);
-		if (!flag_xml_read)
-		{
-			exit(-1);
-		}
-		hPen = CreatePen(PS_SOLID, linewidth, RGB(color[0], color[1], color[2]));
-		SelectObject(hdc, hPen);
-		switch (type_drawing)
-		{
-			// TODO: 
-			// 1:非最大化窗口时，画图的位置会有偏差
-			// 2:实时显示画图过程，而非两个点相连
-			case TYPE_LINE:
-				MoveToEx(hdc, ptBegin.x, ptBegin.y, NULL);
-				LineTo(hdc, ptEnd.x, ptEnd.y);
-				break;
-			case TYPE_RECT:
-				Rectangle(hdc, ptBegin.x, ptBegin.y, ptEnd.x, ptEnd.y);
-				break;
-			case TYPE_ELLIPSE:
-				Ellipse(hdc, ptBegin.x, ptBegin.y, ptEnd.x, ptEnd.y);
-				break;
-			default:
-				break;
-		}
-		DeleteObject(hPen);
-		ReleaseDC(hwnd, hdc);
+		ptShapeEnd.x = LOWORD(lParam);
+		ptShapeEnd.y = HIWORD(lParam);
+		//GetCursorPos(&ptShapeEnd); //获取坐标不准确
+		
+		InvalidateRect(hwnd, NULL, TRUE); //关键，使区域无效化，可以调用WM_PAINT函数，不然画不出来
 		return 0;
-	case WM_RBUTTONDOWN:
+		
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		SaveDrawing(hwnd, hdc, ptShapeBegin, ptShapeEnd, draw_parm, type_shape, type_color, type_linewidth);
+		EndPaint(hwnd, &ps);
+		return 0;
+	/*case WM_RBUTTONDOWN:
 		hdc = GetDC(hwnd);
 		GetClientRect(hwnd, &rect);
 		ReleaseDC(hwnd, hdc);
 		InvalidateRect(hwnd, NULL, true);
 		return 0;
 	case WM_RBUTTONUP:
-		return 0;
+		return 0;*/
 	case WM_COMMAND:
 		wmId = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
 		switch (wmId)
 		{
-			case ID_TYPE_LINE:
-				type_drawing = TYPE_LINE;
-				break;
-			case ID_TYPE_RECTANGLE:
-				type_drawing = TYPE_RECT;
-				break;
-			case ID_TYPE_ELLIPSE:
-				type_drawing = TYPE_ELLIPSE;
-				break;
-			case ID_COLOR_RED:
-				type_color = TYPE_RED;
-				break;
-			case ID_COLOR_BLUE:
-				type_color = TYPE_BLUE;
-				break;
-			case ID_COLOR_GR:
-				type_color = TYPE_GREEN;
-				break;
-			case ID_LINEWIDTH_THIN:
-				type_linewidth = TYPE_THIN;
-				break;
-			case ID_LINEWIDTH_MEDIUM:
-				type_linewidth = TYPE_MEDIUM;
-				break;
-			case ID_LINEWIDTH_THICK:
-				type_linewidth = TYPE_THICK;
-				break;
-			default:
-				break;
+		case ID_TYPE_LINE:
+			type_shape = TYPE_LINE;
+			break;
+		case ID_TYPE_RECTANGLE:
+			type_shape = TYPE_RECT;
+			break;
+		case ID_TYPE_ELLIPSE:
+			type_shape = TYPE_ELLIPSE;
+			break;
+		case ID_COLOR_RED:
+			type_color = TYPE_RED;
+			break;
+		case ID_COLOR_BLUE:
+			type_color = TYPE_BLUE;
+			break;
+		case ID_COLOR_GR:
+			type_color = TYPE_GREEN;
+			break;
+		case ID_LINEWIDTH_THIN:
+			type_linewidth = TYPE_THIN;
+			break;
+		case ID_LINEWIDTH_MEDIUM:
+			type_linewidth = TYPE_MEDIUM;
+			break;
+		case ID_LINEWIDTH_THICK:
+			type_linewidth = TYPE_THICK;
+			break;
+		default:
+			break;
 			return  DefWindowProc(hwnd, message, wParam, lParam);
 		}
-		return 0;
-	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
-		GetClientRect(hwnd, &rect);
-		DrawText(hdc, TEXT("Please move leftbutton to draw, click rightbutton to erase!"), 59, &rect, DT_CENTER | DT_SINGLELINE);
-		EndPaint(hwnd, &ps);
 		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
