0ab452b1fe038590e95dc3a8df4cffe07e77036f
P1ckCheN <849566149@qq.com>
2021-06-21 15:21:48 +0800

    Version 2.0: Improve the function

--- include/freepainting.h
+++ include/freepainting.h
@@ -2,10 +2,10 @@
  * \file freepainting.h
  * \brief 主程序头文件
  *
- * 定义结构体
+ * 定义结构体变量和全局变量
  *
  * \author 陈瑞佳
- * \version 1.0
+ * \version 1.2
  * \date 2021/06/17
  */
 
@@ -17,38 +17,61 @@
 #include "Resource.h"
 #include "parsingxml.h"
 
-// 定义参数结构体
+ // 定义参数结构体
 enum ShapeParm
-{
-	TYPE_LINE,						///< 形状：直线	
-	TYPE_RECT,						///< 形状：矩形
-	TYPE_ELLIPSE,					///< 形状：椭圆
+{	
+	TYPE_LINE,													///< 形状：直线	
+	TYPE_RECT,													///< 形状：矩形
+	TYPE_ELLIPSE,												///< 形状：椭圆
 };
 
 enum ColorParm
 {
-	TYPE_RED,						///< 颜色：红色
-	TYPE_BLUE,						///< 颜色：蓝色
-	TYPE_GREEN,						///< 颜色：绿色
+	TYPE_RED,													///< 颜色：红色
+	TYPE_BLUE,													///< 颜色：蓝色
+	TYPE_GREEN,													///< 颜色：绿色
 };
 
 enum LinewidthParm
 {
-	TYPE_THIN,						///< 粗细度：细
-	TYPE_MEDIUM,					///< 粗细度：中
-	TYPE_THICK						///< 粗细度：粗
+	TYPE_THIN,													///< 粗细度：细
+	TYPE_MEDIUM,												///< 粗细度：中
+	TYPE_THICK													///< 粗细度：粗
 };
 
-struct DrawParm
+struct DrawParm	 
 {
-	POINT ptBegin;
-	POINT ptEnd;
-	ShapeParm shape_parm;
-	//ColorParm color_parm;
-	//LinewidthParm LinewidthParm;
-	std::vector<int> color_parm;
-	int LinewidthParm;
+	POINT ptBegin;												///< 起始点
+	POINT ptEnd;												///< 终止点
+	ShapeParm shape_parm;										///< 形状
+	std::vector<int> color_parm;								///< 颜色
+	int LinewidthParm;											///< 线条宽度
 };
-void SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd,
+
+// 运行时所需全局变量
+static bool flag_clear;											///< 清除画板
+static bool flag_readxmlfile;									///< 是否读取文件
+
+static POINT ptShapeBegin;										///< 起始点
+static POINT ptShapeEnd;										///< 终止点
+
+static ShapeParm type_shape;									///< 形状类型
+static ColorParm type_color;									///< 颜色类型
+static LinewidthParm type_linewidth;							///< 线条宽度类型
+static std::vector<DrawParm> draw_parm;							///< 保存历史绘制数组
+
+static int color[3];											///< 颜色值
+static int linewidth;											///< 线条宽度值
+
+static std::unordered_map<std::string, int*> color_cache;		///< 哈希表，对应颜色映射
+static std::unordered_map<std::string, int> linewidth_cache;	///< 哈希表，对应线条宽度映射
+
+// 函数定义
+VOID SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd,
 	std::vector<DrawParm>& draw_parm, ShapeParm type_shape, ColorParm type_color, LinewidthParm type_linewidth);
-#endif
+
+VOID ClearDrawing(std::vector<DrawParm>& draw_parm);
+
+VOID ThreadReadXmlFile(PVOID pvoid);
+
+#endif // !FREEPAINTING_H__
--- src/freepainting.cpp
+++ src/freepainting.cpp
@@ -3,13 +3,24 @@
  * \brief 程序入口
  *
  * 主程序
- * v1.1 
+ * v1.1 2021.6.19
  * (1) 修复绘图时起始点和终点不准的问题
  * (2) 修复放大缩小窗口会擦除痕迹的问题
  * (3) 更好地区分三种不同的绘图参数结构
+ * 
+ * v1.2 2021.6.20
+ * (1) 更改文字提示框显示位置
+ * (2) 修正右键点击无法清屏问题
+ * (3) 可以控制是否填充图像
+ * (4) 使用MB弹出错误，更直观
+ * 
+ * v2.0 2021.6.21
+ * (1) 加入多线程读取xml方法，便于文件更新
+ * (2) 完善error管理机制
+ * 
  * \author 陈瑞佳
- * \version 1.1
- * \date 2021/06/20
+ * \version 2.0
+ * \date 2021/06/21
  */
 
 
@@ -18,7 +29,11 @@
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 /**
- * @brief 窗口主程序
+ * @brief WIN32窗口主程序
+ * @param [in] hInstance		程序当前运行的实例句柄
+ * @param [in] hPreInstance		前一个实例句柄，NULL
+ * @param [in] szCmdLine		传递给程序的命令行参数
+ * @param [in] iCmdShow			窗口显示的方式
  */
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR szCmdLine, _In_ int iCmdShow)
 {
@@ -44,8 +59,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
 	if (!RegisterClassW(&wndclass))
 	{
-		MessageBox(NULL, TEXT("Program requires Windows NT!"),
-			szAppName, MB_ICONERROR);
+		ErrorShow(ERROR_REGISTER_CLASS);
 		return 0;
 	}
 
@@ -64,22 +78,31 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	return msg.wParam;
 }
 
-void SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd, 
+/**
+ * @brief 画板，每次刷新窗口重新绘制历史图像
+ * @param [in] hwnd				窗体句柄
+ * @param [in] hdc				图像设备描述表
+ * @param [in] ptBegin			绘画起始点
+ * @param [in] ptEnd			绘画终止点
+ * @param [in] type_shape		菜单选择的线条形状
+ * @param [in] type_color		菜单选择的图形颜色
+ * @param [in] type_linewidth	菜单选择的线条宽度
+ * @param [out] draw_parm		绘制所需参数数组
+ */
+VOID SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd, 
 	std::vector<DrawParm>& draw_parm, ShapeParm type_shape, ColorParm type_color, LinewidthParm type_linewidth)
 {
 	RECT rect; //使用参数NULL代替，表示整个屏幕
 	HPEN hpen;
-	int color[3];
-	int linewidth;
-
+	
 	GetClientRect(hwnd, &rect);
 	DrawText(hdc, TEXT("Please move leftbutton to draw, click rightbutton to erase!"), 59, &rect, DT_CENTER | DT_SINGLELINE);
+	
 	// TODO:完善异常处理机制
-	int flag_xml_read = ReadXmlFile(type_color, type_linewidth, color, &linewidth);
-	if (!flag_xml_read)
-	{
-		exit(-1);
-	}
+	
+	if(flag_readxmlfile)
+		ErrorShow(ParsingData(type_color, type_linewidth, color, linewidth, color_cache, linewidth_cache));
+
 	DrawParm curr_parm;
 	curr_parm.ptBegin = ptBegin;
 	curr_parm.ptEnd = ptEnd;
@@ -92,15 +115,15 @@ void SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd,
 
 	draw_parm.emplace_back(curr_parm);
 	
-	// TODO: 用循环画图出现速度很慢，图数量多会出现画图帧数人眼可分辨的情况
+	// TODO: 用循环画图速度慢，即图数量很多会出现画图帧数不够的情况
 	for (int i = 0; i < draw_parm.size(); i++)
 	{
 		hpen = CreatePen(PS_SOLID, draw_parm[i].LinewidthParm, RGB(draw_parm[i].color_parm[0], draw_parm[i].color_parm[1], draw_parm[i].color_parm[2]));
 		SelectObject(hdc, hpen);
+		SelectObject(hdc, GetStockObject(NULL_BRUSH)); // 该行可以控制是否画出填充图像
 		switch (draw_parm[i].shape_parm)
 		{
-			// TODO: 
-			// 2:实时显示画图过程，而非两个点相连
+			// TODO: 实时显示画图过程，而非两个点相连
 		case TYPE_LINE:
 			MoveToEx(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, NULL);
 			LineTo(hdc, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
@@ -120,29 +143,50 @@ void SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd,
 	
 }
 
-void ClearDrawing(std::vector<DrawParm>& draw_parm)
+
+/**
+ * @brie 清除历史图像
+ * @param [in] draw_parm		绘制所需参数数组
+ */
+VOID ClearDrawing(std::vector<DrawParm>& draw_parm)
 {
 	draw_parm.clear();
 	return;
 }
+
+/**
+ * @brie 多线程读取文件
+ * @param [in] pvoid			线程指针
+ */
+VOID ThreadReadXmlFile(PVOID pvoid)
+{
+	while (TRUE)
+	{
+		ErrorShow(ReadXmlFile(color_cache, linewidth_cache));
+		flag_readxmlfile = true;
+		// TODO:增加定时器功能？
+		Sleep(10 * 1000); //延迟10s
+	}
+}
+
 /**
  * @brief 消息回调函数
+ * @param [in] hwnd			窗体句柄
+ * @param [in] message			消息常量标识符
+ * @param [in] wParam			消息有关的常量值，或者窗口和控件的句柄
+ * @param [in] lParam			指向内存中数据的指针
  */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
 	int wmId, wmEvent;
-	static int flag_clear = false;
-	static POINT ptShapeBegin, ptShapeEnd;
-
-	static ShapeParm type_shape;								///< 默认形状：直线
-	static ColorParm type_color;								///< 默认色彩：红色
-	static LinewidthParm type_linewidth;						///< 默认粗细度：细
-	static std::vector<DrawParm> draw_parm;
 	int flag_xml_read = 0;
 	switch (message)
 	{
+	case WM_CREATE:
+		_beginthread(ThreadReadXmlFile, 0, NULL);
+		return 0;
 	case WM_LBUTTONDOWN:
 		ptShapeBegin.x = LOWORD(lParam);
 		ptShapeBegin.y = HIWORD(lParam);
@@ -155,7 +199,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		
 		InvalidateRect(hwnd, NULL, TRUE); //关键，使区域无效化，可以调用WM_PAINT函数，不然画不出来
 		return 0;
-		
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
 		if (flag_clear == false)
