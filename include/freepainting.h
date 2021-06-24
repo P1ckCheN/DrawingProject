1b376c3f4217046367f72a8082496c1fc1ddc56e
P1ckCheN <849566149@qq.com>
2021-06-25 02:22:58 +0800

    Version 2.2: Improve reading and writing functions

--- include/freepainting.h
+++ include/freepainting.h
@@ -5,73 +5,134 @@
  * 定义结构体变量和全局变量
  *
  * \author 陈瑞佳
- * \version 1.2
- * \date 2021/06/17
+ * \version 2.2
+ * \date 2021/06/24
  */
 
 #ifndef FREEPAINTING_H__
 #define FREEPAINTING_H__
 
-
 #include "framework.h"
 #include "Resource.h"
-#include "parsingxml.h"
 
- // 定义参数结构体
-enum ShapeParm
+ // 画图参数
+enum class ShapeParm
 {	
 	TYPE_LINE,													///< 形状：直线	
 	TYPE_RECT,													///< 形状：矩形
-	TYPE_ELLIPSE,												///< 形状：椭圆
+	TYPE_ELLIPSE												///< 形状：椭圆
 };
 
-enum ColorParm
+enum class ColorParm
 {
 	TYPE_RED,													///< 颜色：红色
 	TYPE_BLUE,													///< 颜色：蓝色
-	TYPE_GREEN,													///< 颜色：绿色
+	TYPE_GREEN													///< 颜色：绿色
 };
 
-enum LinewidthParm
+enum class LinewidthParm
 {
 	TYPE_THIN,													///< 粗细度：细
 	TYPE_MEDIUM,												///< 粗细度：中
 	TYPE_THICK													///< 粗细度：粗
 };
 
-struct DrawParm	 
+// 存储结构体
+struct DrawingParm	 
 {
-	POINT ptBegin;												///< 起始点
-	POINT ptEnd;												///< 终止点
+	POINT point_begin;											///< 起始点
+	POINT point_end;											///< 终止点
 	ShapeParm shape_parm;										///< 形状
 	std::vector<int> color_parm;								///< 颜色
-	int LinewidthParm;											///< 线条宽度
+	int linewidth_parm;											///< 线条宽度
 };
 
-// 运行时所需全局变量
-static bool flag_clear;											///< 清除画板
-static bool flag_readxmlfile;									///< 是否读取文件
+enum class DataState
+{
+	NO_UPDATE = 0,												///< 未更新
+	UPDATEED = 1,												///< 已更新
+	DELETED = 2													///< 已删除
+};
 
-static POINT ptShapeBegin;										///< 起始点
-static POINT ptShapeEnd;										///< 终止点
+// 模拟存储类
+class Cache
+{
+public:
+	// 成员变量
+	DataState state;											///< 当前状态
+	std::unordered_map<std::string, int*> color_cache;			///< 颜色存储
+	std::unordered_map<std::string, int > linewidth_cache;		///< 线条宽度存储
 
-static ShapeParm type_shape;									///< 形状类型
-static ColorParm type_color;									///< 颜色类型
-static LinewidthParm type_linewidth;							///< 线条宽度类型
-static std::vector<DrawParm> draw_parm;							///< 保存历史绘制数组
+	// 成员函数
+	Cache();													///< 构造函数
+	void deletecache();											///< 删除缓存
+	Cache& operator=(const Cache& cache);						///< 重载赋值运算符
+};
 
-static int color[3];											///< 颜色值
-static int linewidth;											///< 线条宽度值
 
-static std::unordered_map<std::string, int*> color_cache;		///< 哈希表，对应颜色映射
-static std::unordered_map<std::string, int> linewidth_cache;	///< 哈希表，对应线条宽度映射
 
-// 函数定义
-VOID SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd,
-	std::vector<DrawParm>& draw_parm, ShapeParm type_shape, ColorParm type_color, LinewidthParm type_linewidth);
+// 画板类
+class DrawingBoard
+{
+private:
+	// 成员变量
+	bool flag_clear;										    ///< 清除画板
+	bool flag_read_xml_file;									///< 是否已经读取文件
 
-VOID ClearDrawing(std::vector<DrawParm>& draw_parm);
+	int color[3];												///< 画板对象颜色
+	int linewidth;												///< 画板对象线条宽度
 
-VOID ThreadReadXmlFile(PVOID pvoid);
+	POINT point_begin;											///< 画板对象鼠标起始点
+	POINT point_end;											///< 画板对象鼠标终止点
+
+	ShapeParm type_shape;										///< 线条类型
+	ColorParm type_color;										///< 颜色类型
+	LinewidthParm type_linewidth;								///< 线条宽度类型
+	std::vector<DrawingParm> drawing_parm;						///< 参数存储结构体
+
+public:
+	// 成员函数
+	DrawingBoard() :	
+		flag_clear(false), flag_read_xml_file(false),
+		color{ 255,0,0 }, linewidth(0),
+		point_begin{ 0, 0 }, point_end{ 0, 0 },
+		type_shape(ShapeParm::TYPE_LINE), type_color(ColorParm::TYPE_RED), type_linewidth(LinewidthParm::TYPE_THIN) {}
+	~DrawingBoard();
+	void Drawing(HWND hwnd, HDC hdc);
+	void ClearDrawing();
+	inline void SetType(ShapeParm shape) { type_shape = shape; }
+	inline void SetColor(ColorParm color) { type_color = color; }
+	inline void SetLinewidth(LinewidthParm linewidth) { type_linewidth = linewidth; }
+	inline void SetRead(bool flag) { flag_read_xml_file = flag; }
+	inline bool GetClear() { return flag_clear; }
+	inline void SetClear() { flag_clear = true; }
+	inline void SetCurrPoint(POINT point_curr_begin, POINT point_curr_end)
+	{
+		point_begin = point_curr_begin;
+		point_end = point_curr_end;
+	}
+};
+
+// 全局变量
+static DrawingBoard board;											///< 画板对象
+static POINT point_curr_begin;										///< 起始点
+static POINT point_curr_end;										///< 终止点
+static Cache color_linewidth_cache;									///< 缓存
+static Cache color_linewidth_database;								///< 数据库
+	
+// 函数声明
+void ThreadReadXmlFile(DrawingBoard& board);
+
+void ReadXmlFile1(DrawingBoard& board);
+
+void Transform(int* color_value, std::string& color_value_string);
+
+int ParsingDataCache(ColorParm in_color, LinewidthParm in_linewidth, int* out_color, int& out_linewidth,
+	std::unordered_map<std::string, int*>& color_cache, std::unordered_map<std::string, int>& linewidth_cache);
+
+int ParsingDataAtomic(ColorParm in_color, LinewidthParm in_linewidth, int* out_color, int& out_linewidth,
+	std::unordered_map<std::string, std::atomic_int*>& color_cache, std::unordered_map<std::string, std::atomic_int>& linewidth_cache);
+
+int ReadXmlFile(DrawingBoard& board);
 
 #endif // !FREEPAINTING_H__
--- src/freepainting.cpp
+++ src/freepainting.cpp
@@ -12,17 +12,28 @@
  * (1) 更改文字提示框显示位置
  * (2) 修正右键点击无法清屏问题
  * (3) 可以控制是否填充图像
- * (4) 使用MB弹出错误，更直观
  * 
  * v2.0 2021.6.21
  * (1) 加入多线程读取xml方法，便于文件更新
- * (2) 完善error管理机制
+ * (2) 使用MB弹出错误，更直观
+ * (3) 完善error管理机制
+ * 
+ * v2.1 2021.6.23
+ * (1) 添加画板类
+ * (2) 添加多线程访问控制，原子量方法
+ * 
+ * v2.2 2021.6.23
+ * (1) 添加数据存储类
+ * (1) 增设数据库 + 缓存 Cache Aside Pattern 方法
+ * 读的时候，先读缓存;
+ * 如果没有缓存, 读数据库, 取出数据放入缓存, 同时返回响应;
+ * 更新的时候, 先更新数据库, 再删除缓存
+
  * 
  * \author 陈瑞佳
- * \version 2.0
- * \date 2021/06/21
+ * \version 2.2
+ * \date 2021/06/24
  */
-
 
 #include "freepainting.h"
 
@@ -42,7 +53,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	MSG msg;
 	static HBRUSH hBrush;
 	WNDCLASS wndclass;
-
+	// 开启读取文件的线程
+	std::thread thread_read_xml_file(ThreadReadXmlFile, std::ref(board));
+	thread_read_xml_file.detach();
+	
 	LoadMenu(hInstance, TEXT("Menu"));
 	hBrush = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
 
@@ -79,99 +93,143 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 }
 
 /**
- * @brief 画板，每次刷新窗口重新绘制历史图像
+ * @brief DrawingBoard类，析构函数
+ */
+DrawingBoard::~DrawingBoard()
+{
+	drawing_parm.clear();
+}
+
+/**
+ * @brief Cache类，构造函数
+ * 初始化
+ */
+Cache::Cache()
+{
+	state = DataState::NO_UPDATE;
+	color_cache.clear();
+	color_cache.clear();
+}
+
+/**
+ * @brief Cache类, 构造函数
+ * 初始化
+ */
+void Cache::deletecache()
+{
+	state = DataState::DELETED;
+	color_cache.clear();
+	linewidth_cache.clear();
+}
+
+/**
+ * @brief Cache类, 拷贝构造函数
+ * 相比于默认拷贝构造,因为只需要数据->缓存
+ * 所以不拷贝状态
+ * 初始化
+ */
+Cache& Cache::operator=(const Cache& cache)
+{
+	color_cache = cache.color_cache;
+	linewidth_cache = cache.linewidth_cache;
+	return *this;
+}
+
+/**
+ * @brief DrawingBoard类, 画图函数
  * @param [in] hwnd				窗体句柄
  * @param [in] hdc				图像设备描述表
- * @param [in] ptBegin			绘画起始点
- * @param [in] ptEnd			绘画终止点
- * @param [in] type_shape		菜单选择的线条形状
- * @param [in] type_color		菜单选择的图形颜色
- * @param [in] type_linewidth	菜单选择的线条宽度
- * @param [out] draw_parm		绘制所需参数数组
  */
-VOID SaveDrawing(HWND hwnd, HDC hdc, POINT ptBegin, POINT ptEnd, 
-	std::vector<DrawParm>& draw_parm, ShapeParm type_shape, ColorParm type_color, LinewidthParm type_linewidth)
+void DrawingBoard::Drawing(HWND hwnd, HDC hdc)
 {
-	RECT rect; //使用参数NULL代替，表示整个屏幕
+	RECT rect;
 	HPEN hpen;
-	
 	GetClientRect(hwnd, &rect);
 	DrawText(hdc, TEXT("Please move leftbutton to draw, click rightbutton to erase!"), 59, &rect, DT_CENTER | DT_SINGLELINE);
-	
-	// TODO:完善异常处理机制
-	
-	if(flag_readxmlfile)
-		ErrorShow(ParsingData(type_color, type_linewidth, color, linewidth, color_cache, linewidth_cache));
 
-	DrawParm curr_parm;
-	curr_parm.ptBegin = ptBegin;
-	curr_parm.ptEnd = ptEnd;
-	curr_parm.shape_parm = type_shape;
-	for (int i = 0; i < 3; i++)
+	// 跳过最开始未读入数据库且未写入缓存的情况
+	while (true)
 	{
-		curr_parm.color_parm.push_back(color[i]);
+		if (color_linewidth_cache.state != DataState::NO_UPDATE)
+			break;
 	}
-	curr_parm.LinewidthParm = linewidth;
 
-	draw_parm.emplace_back(curr_parm);
-	
-	// TODO: 用循环画图速度慢，即图数量很多会出现画图帧数不够的情况
-	for (int i = 0; i < draw_parm.size(); i++)
+	// 如果满足缓存不存在,则将数据库拷贝到缓存
+	if (color_linewidth_cache.state == DataState::DELETED)
 	{
-		hpen = CreatePen(PS_SOLID, draw_parm[i].LinewidthParm, RGB(draw_parm[i].color_parm[0], draw_parm[i].color_parm[1], draw_parm[i].color_parm[2]));
+		color_linewidth_cache = color_linewidth_database;
+		color_linewidth_cache.state = DataState::UPDATEED;
+	}
+	
+	// 使用缓存数据参数绘图
+	ErrorShow(ParsingDataCache(type_color, type_linewidth, color, linewidth, color_linewidth_cache.color_cache , color_linewidth_cache.linewidth_cache));
+
+	// 下面为之前版本的原子实现
+	//if (flag_read_xml_file)
+		//ErrorShow(ParsingDataAtomic(type_color, type_linewidth, color, linewidth, color_cache_thread, linewidth_cache_thread));
+	
+	DrawingParm curr_parm;
+	curr_parm.point_begin = point_begin;
+	curr_parm.point_end = point_end;
+	curr_parm.shape_parm = type_shape;
+	curr_parm.color_parm = {color[0], color[1], color[2]};
+	curr_parm.linewidth_parm = linewidth;
+
+	drawing_parm.emplace_back(curr_parm); // 保存历史绘图
+
+	for (DrawingParm& his_parm : drawing_parm)
+	{
+		hpen = CreatePen(PS_SOLID, his_parm.linewidth_parm, RGB(his_parm.color_parm[0], his_parm.color_parm[1], his_parm.color_parm[2]));
 		SelectObject(hdc, hpen);
 		SelectObject(hdc, GetStockObject(NULL_BRUSH)); // 该行可以控制是否画出填充图像
-		switch (draw_parm[i].shape_parm)
+		switch (his_parm.shape_parm)
 		{
 			// TODO: 实时显示画图过程，而非两个点相连
-		case TYPE_LINE:
-			MoveToEx(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, NULL);
-			LineTo(hdc, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
+		case ShapeParm::TYPE_LINE:
+			MoveToEx(hdc, his_parm.point_begin.x, his_parm.point_begin.y, NULL);
+			LineTo(hdc, his_parm.point_end.x, his_parm.point_end.y);
 			break;
-		case TYPE_RECT:
-			Rectangle(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
+		case ShapeParm::TYPE_RECT:
+			Rectangle(hdc, his_parm.point_begin.x, his_parm.point_begin.y, his_parm.point_end.x, his_parm.point_end.y);
 			break;
-		case TYPE_ELLIPSE:
-			Ellipse(hdc, draw_parm[i].ptBegin.x, draw_parm[i].ptBegin.y, draw_parm[i].ptEnd.x, draw_parm[i].ptEnd.y);
+		case ShapeParm::TYPE_ELLIPSE:
+			Ellipse(hdc, his_parm.point_begin.x, his_parm.point_begin.y, his_parm.point_end.x, his_parm.point_end.y);
 			break;
 		default:
 			break;
 		}
 		DeleteObject(hpen);
 	}
-
-	
 }
 
-
 /**
- * @brie 清除历史图像
- * @param [in] draw_parm		绘制所需参数数组
+ * @brief DrawingBoard类,清除屏幕
+ * 右键清屏实现
  */
-VOID ClearDrawing(std::vector<DrawParm>& draw_parm)
+void DrawingBoard::ClearDrawing()
 {
-	draw_parm.clear();
+	flag_clear = false;
+	drawing_parm.clear();
 	return;
 }
 
 /**
  * @brie 多线程读取文件
- * @param [in] pvoid			线程指针
+ * @param [in] board			画板对象
  */
-VOID ThreadReadXmlFile(PVOID pvoid)
+
+void ThreadReadXmlFile(DrawingBoard& board)
 {
-	while (TRUE)
+	while (true)
 	{
-		ErrorShow(ReadXmlFile(color_cache, linewidth_cache));
-		flag_readxmlfile = true;
-		// TODO:增加定时器功能？
-		Sleep(10 * 1000); //延迟10s
+		ErrorShow(ReadXmlFile(board));
+		Sleep(10 * 1000);
 	}
 }
 
 /**
  * @brief 消息回调函数
- * @param [in] hwnd			窗体句柄
+ * @param [in] hwnd				窗体句柄
  * @param [in] message			消息常量标识符
  * @param [in] wParam			消息有关的常量值，或者窗口和控件的句柄
  * @param [in] lParam			指向内存中数据的指针
@@ -185,38 +243,37 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_CREATE:
-		_beginthread(ThreadReadXmlFile, 0, NULL);
+		//_beginthread(ThreadReadXmlFile, 0, NULL);	std::thread thread_read_xml_file(ThreadReadXmlFile);
 		return 0;
 	case WM_LBUTTONDOWN:
-		ptShapeBegin.x = LOWORD(lParam);
-		ptShapeBegin.y = HIWORD(lParam);
+		point_curr_begin.x = LOWORD(lParam);
+		point_curr_begin.y   = HIWORD(lParam);
 		//GetCursorPos(&ptShapeBegin); //获取坐标不准确
 		return 0;
 	case WM_LBUTTONUP:
-		ptShapeEnd.x = LOWORD(lParam);
-		ptShapeEnd.y = HIWORD(lParam);
+		point_curr_end.x = LOWORD(lParam);
+		point_curr_end.y   = HIWORD(lParam);
 		//GetCursorPos(&ptShapeEnd); //获取坐标不准确
-		
 		InvalidateRect(hwnd, NULL, TRUE); //关键，使区域无效化，可以调用WM_PAINT函数，不然画不出来
 		return 0;
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
-		if (flag_clear == false)
+		if (board.GetClear() == false)
 		{
-			SaveDrawing(hwnd, hdc, ptShapeBegin, ptShapeEnd, draw_parm, type_shape, type_color, type_linewidth);
+			board.SetCurrPoint(point_curr_begin, point_curr_end);
+			board.Drawing(hwnd, hdc);
 		}
 		else
 		{
 			RECT rect;
 			GetClientRect(hwnd, &rect);
 			DrawText(hdc, TEXT("Please move leftbutton to draw, click rightbutton to erase!"), 59, &rect, DT_CENTER | DT_SINGLELINE);
-			ClearDrawing(draw_parm);
-			flag_clear = false;
+			board.ClearDrawing();
 		}
 		EndPaint(hwnd, &ps);
 		return 0;
 	case WM_RBUTTONDOWN:
-		flag_clear = true;
+		board.SetClear();
 		return 0;
 	case WM_RBUTTONUP:
 		InvalidateRect(hwnd, NULL, TRUE);
@@ -227,31 +284,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		switch (wmId)
 		{
 		case ID_TYPE_LINE:
-			type_shape = TYPE_LINE;
+			board.SetType(ShapeParm::TYPE_LINE);
 			break;
 		case ID_TYPE_RECTANGLE:
-			type_shape = TYPE_RECT;
+			board.SetType(ShapeParm::TYPE_RECT);
 			break;
 		case ID_TYPE_ELLIPSE:
-			type_shape = TYPE_ELLIPSE;
+			board.SetType(ShapeParm::TYPE_ELLIPSE);
 			break;
 		case ID_COLOR_RED:
-			type_color = TYPE_RED;
+			board.SetColor(ColorParm::TYPE_RED);
 			break;
 		case ID_COLOR_BLUE:
-			type_color = TYPE_BLUE;
+			board.SetColor(ColorParm::TYPE_BLUE);
 			break;
 		case ID_COLOR_GR:
-			type_color = TYPE_GREEN;
+			board.SetColor(ColorParm::TYPE_GREEN);
 			break;
 		case ID_LINEWIDTH_THIN:
-			type_linewidth = TYPE_THIN;
+			board.SetLinewidth(LinewidthParm::TYPE_THIN);
 			break;
 		case ID_LINEWIDTH_MEDIUM:
-			type_linewidth = TYPE_MEDIUM;
+			board.SetLinewidth(LinewidthParm::TYPE_MEDIUM);
 			break;
 		case ID_LINEWIDTH_THICK:
-			type_linewidth = TYPE_THICK;
+			board.SetLinewidth(LinewidthParm::TYPE_THICK);
 			break;
 		default:
 			break;
@@ -259,8 +316,89 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		return 0;
 	case WM_DESTROY:
+		//_endthread(); // 线程退出代码？
 		PostQuitMessage(0);
 		return 0;
 	}
 	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+/**
+ * @brief 读取XML文件
+ * version 2.2 
+ * 采用数据库+缓存形式
+ * @param [in]  board		画板对象
+ * @param [out] board		画板对象
+ */
+int ReadXmlFile(DrawingBoard& board)
+{
+	// 先更新数据库
+	color_linewidth_database.state = DataState::NO_UPDATE;
+
+	std::string color_value_string;
+	std::string linewidth_value_string;
+	int* color_transform = new int[3];
+
+	//打开文件
+	TiXmlDocument doc;
+	if (!doc.LoadFile("xml/config.xml"))
+	{
+		return ERROR_XML_PATH;
+	}
+	TiXmlElement* xml_configure = doc.RootElement();
+	if (!xml_configure)
+	{
+		return ERROR_XML_CONFIGURE;
+	}
+	TiXmlElement* xml_color_configure = xml_configure->FirstChildElement("color_configure");
+	TiXmlElement* xml_linewidth_configure = xml_color_configure->NextSiblingElement("linewidth_configure");
+
+	bool flag_get_color = 0;
+	bool flag_get_linewidth = 0;
+
+	// 将颜色存入数据库
+	TiXmlElement* xml_color = xml_color_configure->FirstChildElement("color");
+	while (xml_color != nullptr)
+	{
+		TiXmlElement* xml_color_name = xml_color->FirstChildElement("name");
+		std::string s = xml_color_name->GetText();
+
+		color_value_string = xml_color_name->NextSiblingElement("value")->GetText();
+		Transform(color_transform, color_value_string);
+
+		color_linewidth_database.color_cache[s] = new int[3];
+		for (int i = 0; i < 3; i++)
+		{
+			color_linewidth_database.color_cache[s][i] = color_transform[i];
+		}
+
+		xml_color = xml_color->NextSiblingElement();
+	}
+
+	// 将线条存入数据库
+	TiXmlElement* xml_linewidth = xml_linewidth_configure->FirstChildElement("linewidth");
+	while (xml_linewidth != nullptr)
+	{
+		TiXmlElement* xml_linewidth_name = xml_linewidth->FirstChildElement("name");
+		std::string s = xml_linewidth_name->GetText();
+		color_linewidth_database.linewidth_cache[s] = std::stoi(xml_linewidth_name->NextSiblingElement("value")->GetText());
+		xml_linewidth = xml_linewidth->NextSiblingElement();
+	}
+
+	// 异常判断
+	if (color_linewidth_database.color_cache.empty())
+	{
+		return ERROR_EMPTY_COLOR;
+	}
+	if (color_linewidth_database.linewidth_cache.empty())
+	{
+		return ERROR_EMPTY_LINEWIDTH;
+	}
+
+	//数据库已更新
+	color_linewidth_database.state = DataState::UPDATEED;
+	// 删除缓存
+	color_linewidth_cache.deletecache();
+	board.SetRead(true);
+	return SUCCESS;
 }
